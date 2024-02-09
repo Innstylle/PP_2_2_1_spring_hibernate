ca06ed3678c83f2e3f371d28f512a6716cdc44d1
Kirill <vkashe1337@gmail.com>
2024-02-09 19:10:34 +0300

    Исправлены замечания.

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -6,41 +6,37 @@ import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.sql.SQLException;
 import java.util.List;
 
 public class MainApp {
-   public static void main(String[] args) {
-      AnnotationConfigApplicationContext context = 
-            new AnnotationConfigApplicationContext(AppConfig.class);
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
 
-      UserService userService = context.getBean(UserService.class);
-      User user1 = new User("User1", "Lastname1", "user1@mail.ru");
-      User user2 = new User("User2", "Lastname2", "user2@mail.ru");
-      User user3 = new User("User3", "Lastname3", "user3@gmail.com");
+        UserService userService = context.getBean(UserService.class);
 
-      Car car1 = new Car("Skoda", 10);
-      Car car2 = new Car("Reno", 20);
-      Car car3 = new Car("Opel", 30);
-      user1.setCar(car1);
-      user2.setCar(car2);
-      user3.setCar(car3);
+        Car car1 = new Car("Skoda", 10);
+        Car car2 = new Car("Reno", 20);
+        Car car3 = new Car("Opel", 30);
+        User user1 = new User("User1", "Lastname1", "user1@mail.ru", car1);
+        User user2 = new User("User2", "Lastname2", "user2@mail.ru", car2);
+        User user3 = new User("User3", "Lastname3", "user3@gmail.com", car3);
 
-      userService.add(user1);
-      userService.add(user2);
-      userService.add(user3);
+        userService.add(user1);
+        userService.add(user2);
+        userService.add(user3);
 
-      List<User> userList = userService.listUsers();
-      for (User u : userList) {
-         System.out.println(u);
-      }
+        List<User> userList = userService.listUsers();
+        for (User u : userList) {
+            System.out.println(u);
+        }
 
-      List<User> userListByCar1 = userService.listUsersByModelAndSeries("Reno", "20");
-      System.out.println(userListByCar1);
+        List<User> userListByCar1 = userService.listUsersByModelAndSeries("Reno", "20");
+        System.out.println(userListByCar1);
 
-      List<User> userListByCar2 = userService.listUsersByModelAndSeries("Opel", "30");
-      System.out.println(userListByCar2);
+        List<User> userListByCar2 = userService.listUsersByModelAndSeries("Opel", "30");
+        System.out.println(userListByCar2);
 
-      context.close();
-   }
+        context.close();
+    }
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -11,22 +11,21 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User", User.class);
+        return query.getResultList();
+    }
 
-   @Override
+    @Override
     public List<User> listUsersByModelAndSeries(String model, String series) {
         TypedQuery<User> query = sessionFactory.getCurrentSession().createNamedQuery("User.findByModelAndSeries", User.class);
         query.setParameter("model", model);
